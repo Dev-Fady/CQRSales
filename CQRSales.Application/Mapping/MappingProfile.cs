fc4507a8c9a0db8d1b623a1b4331f339ee8cff4d
Dev-Fady <fadymobiledev@gmail.com>
2026-03-28 15:39:24 +0200

    Mapping and Response done

---
+++ CQRSales.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CQRSales.Application.Features.Commands.CategoryCommands;
+using CQRSales.Application.Features.Dtos.Responses.CategoryResponses;
+using CQRSales.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSales.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category,CategoryReadReponseDto>().ReverseMap();
+            CreateMap<Category, CategoryAddCommands>().ReverseMap();
+        }
+    }
+}
---
+++ CQRSales.Application/Mapping/Response.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSales.Application.Mapping
+{
+    public class Response<T>
+    {
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public bool IsSucceeded { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public Response()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public static Response<T> Success(T data, string message = "Success")
+        {
+            return new Response<T>
+            {
+                Data = data,
+                Message = message,
+                IsSucceeded = true
+            };
+        }
+        public static Response<T> Fail(string message)
+        {
+            return new Response<T>
+            {
+                Message = message,
+                IsSucceeded = false
+            };
+        }
+    }
+}
