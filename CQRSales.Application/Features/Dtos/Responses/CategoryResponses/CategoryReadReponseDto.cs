84f2e8fb216225d29f9744d00234f4bf6b2e60a0
Dev-Fady <fadymobiledev@gmail.com>
2026-03-28 15:38:02 +0200

    Category Read DTO

---
+++ CQRSales.Application/Features/Dtos/Responses/CategoryResponses/CategoryReadReponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSales.Application.Features.Dtos.Responses.CategoryResponses
+{
+    public class CategoryReadReponseDto
+    {
+        public int ID { get; set; }
+        public bool IsArchived { get; set; }
+        public string NameAr { get; set; }
+        public string NameEn { get; set; }
+        public string DescriptionAr { get; set; }
+        public string DescriptionEn { get; set; }
+    }
+}
